fb1ee3af1a5b4bfddf2221195092adc38bb40864
lucaonascimento15 <lsproducoes19@gmail.com>
2025-11-02 13:52:10 -0300

    Troca de selenium para Pyautogui

--- acessar_intranet.py
+++ acessar_intranet.py
@@ -1,41 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from dotenv import load_dotenv
+import pyautogui
 import os, time
 
-load_dotenv()
+pyautogui.PAUSE = 2 
 USER = os.getenv("INTRA_USER")
 PASS = os.getenv("INTRA_PASS")
+link = ("https://www.intranetmall.com/boulevardshoppingvilavelha4/")
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()),
-    options=Options()
-)
 
-driver.get("https://www.intranetmall.com/boulevardshoppingvilavelha4/")
 
-wait = WebDriverWait(driver, 10)
-print("Página:", driver.title)
+pyautogui.press ('win')
+pyautogui.write ('chorme')
+pyautogui.press ('enter')
+pyautogui.write (link)
+pyautogui.press ('enter')
+pyautogui.press ('enter')
 
-try:
-    user_field = wait.until(EC.presence_of_element_located((By.XPATH, "//input[contains(@placeholder,'Usuário') or contains(@id,'user')]")))
-    pass_field = driver.find_element(By.XPATH, "//input[@type='password']")
-    login_btn  = driver.find_element(By.XPATH, "//button[contains(.,'Entrar') or contains(.,'Login')]")
+pyautogui.click()
 
-    user_field.send_keys(USER)
-    pass_field.send_keys(PASS)
-    login_btn.click()
-
-    print("Login enviado.")
-    time.sleep(3)
-    driver.save_screenshot("pos_login.png")
-
-except Exception as e:
-    print("Erro:", e)
-
-driver.quit()
+print("Script realizado com sucesso")
